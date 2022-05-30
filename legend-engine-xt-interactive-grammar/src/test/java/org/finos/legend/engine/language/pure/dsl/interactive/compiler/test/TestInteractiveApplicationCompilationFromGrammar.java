16dcffd5478c7ae7bcd175be57b0b5d3cf1197d9
Aleksas Hauser <aleksas.hauser@gmail.com>
2022-05-30 12:20:37 -0400

    Add xt module for interactive application

--- legend-engine-xt-interactive-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/interactive/compiler/test/TestInteractiveApplicationCompilationFromGrammar.java
+++ legend-engine-xt-interactive-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/interactive/compiler/test/TestInteractiveApplicationCompilationFromGrammar.java
@@ -1,18 +1,21 @@
 package org.finos.legend.engine.language.pure.dsl.interactive.compiler.test;
 
 import org.finos.legend.engine.language.pure.compiler.test.TestCompilationFromGrammar;
+import org.finos.legend.engine.language.pure.dsl.interactive.grammar.test.TestInteractiveApplicationGrammar;
 
-public class TestInteractiveApplicationCompilationFromGrammar extends TestCompilationFromGrammar.TestCompilationFromGrammarTestSuite
+public class TestInteractiveApplicationCompilationFromGrammar extends TestCompilationFromGrammar.TestCompilationFromGrammarTestSuite implements TestInteractiveApplicationGrammar
 {
     @Override
     protected String getDuplicatedElementTestCode()
     {
-        return null;
+        return "Class meta::demo::crud::DemoApp {}\n" +
+                "###InteractiveApplication\n" +
+                this.defaultApplication();
     }
 
     @Override
     protected String getDuplicatedElementTestExpectedErrorMessage()
     {
-        return null;
+        return "COMPILATION error at [3:1-27:1]: Duplicated element 'meta::demo::crud::DemoApp'";
     }
 }
